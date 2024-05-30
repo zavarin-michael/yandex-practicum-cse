897cdebd8e3a43eb1bef4a99d190d31a95d5cce6
Михаил <33755295+zavarin-michael@users.noreply.github.com>
2024-05-30 23:54:46 +0500

    csrf

--- tickets/views.py
+++ tickets/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from tickets.forms import CommentForm
 from tickets.table import UserModel
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
 
 @csrf_exempt
 def get_tickets_page(request):
